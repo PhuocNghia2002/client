71498812c201e67708f4b632766d11564377ea27
truongnghia <truongnghia1209876@gmail.com>
2024-04-16 21:08:10 +0700

    Initial commit

--- src/components/common/TextAvatar.jsx
+++ src/components/common/TextAvatar.jsx
@@ -5,19 +5,20 @@ const TextAvatar = ({ text }) => {
 		let hash = 0;
 		let i;
 
-		for (i = 0; i < str.length; i++) {
+		for (i = 0; i < str.length; i += 1) {
 			hash = str.charCodeAt(i) + ((hash << 5) - hash);
 		}
 
 		let color = "#";
 
-		for (i = 0; i < 3; i++) {
+		for (i = 0; i < 3; i += 1) {
 			const value = (hash >> (i * 8)) & 0xff;
 			color += `00${value.toString(16)}`.slice(-2);
 		}
 
 		return color;
 	};
+
 	return (
 		<Avatar
 			sx={{
@@ -25,7 +26,7 @@ const TextAvatar = ({ text }) => {
 				width: 40,
 				height: 40,
 			}}
-			children={`${text.split(" ")[0][0]}${text.split(" ")[1][0]}`}
+			children={`${text.split(" ")[0][0]}`}
 		/>
 	);
 };
